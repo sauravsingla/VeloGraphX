40a6c6ab6ee2a549592944503c5573e1f310e422
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-08-26 19:18:30 +0530

    test: align NUMA scheduler with partitioner API

--- tests/test_numa_scheduler.cpp
+++ tests/test_numa_scheduler.cpp
@@ -2,7 +2,6 @@
 
 #include <atomic>
 #include <cassert>
-#include <vector>
 
 int main() {
   velographx::NumaInfo info;
@@ -11,9 +10,10 @@ int main() {
   info.native_support = true;
   info.topology = {{0, {0, 2}}, {1, {1, 3}}};
 
-  auto plan = velographx::plan_numa_partitions(100, info, velographx::NumaMode::auto_detect, 4);
+  auto partitions = velographx::plan_numa_vertex_partitions(
+      100, info, velographx::NumaMode::auto_detect, 4);
   velographx::WorkStealingPool pool(4);
-  velographx::NumaLocalScheduler scheduler(pool, plan);
+  velographx::NumaLocalScheduler scheduler(pool, partitions);
 
   assert(scheduler.preferred_queue_for_vertex(0, 0) == 0);
   assert(scheduler.preferred_queue_for_vertex(25, 0) == 1);
@@ -21,8 +21,8 @@ int main() {
   assert(scheduler.preferred_queue_for_vertex(75, 1) == 3);
 
   std::atomic<std::size_t> visited{0};
-  scheduler.parallel_for_partitions([&](const velographx::NumaGraphPartition& partition) {
-    visited.fetch_add(partition.end_vertex - partition.begin_vertex,
+  scheduler.parallel_for_partitions([&](const velographx::NumaVertexPartition& partition) {
+    visited.fetch_add(partition.vertex_end - partition.vertex_begin,
                       std::memory_order_relaxed);
   });
   assert(visited.load(std::memory_order_relaxed) == 100);
