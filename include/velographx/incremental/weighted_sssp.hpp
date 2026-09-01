5b29e3cba4700c5e8c33e7fc1951079d1f0152d2
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-09-01 12:27:23 +0530

    template weighted SSSP on graph access contract

--- include/velographx/incremental/weighted_sssp.hpp
+++ include/velographx/incremental/weighted_sssp.hpp
@@ -5,16 +5,18 @@
 #include <utility>
 #include <vector>
 
+#include "velographx/graph_access.hpp"
 #include "velographx/incremental/dijkstra.hpp"
 #include "velographx/storage/weighted_dynamic_graph.hpp"
 
 namespace velographx {
 
-class IncrementalWeightedSSSP {
+template <class Graph>
+class BasicIncrementalWeightedSSSP {
  public:
   static constexpr std::uint64_t kInf = incremental_detail::kDijkstraInf;
 
-  IncrementalWeightedSSSP(WeightedDynamicGraph& graph, VertexId source)
+  BasicIncrementalWeightedSSSP(Graph& graph, VertexId source)
       : graph_(graph), source_(source) {
     recompute();
   }
@@ -28,29 +30,31 @@ class IncrementalWeightedSSSP {
         requires_recompute = true;
         break;
       }
-      const auto old_weight = graph_.weight(op.src, op.dst);
+      const auto old_weight = edge_weight(graph_, op.src, op.dst);
       if (old_weight && op.weight > *old_weight) {
         requires_recompute = true;
         break;
       }
     }
 
-    graph_.apply(batch);
+    apply_updates(graph_, batch);
     if (requires_recompute) recompute();
     else relax_from_updates(batch);
   }
 
   void recompute() {
     incremental_detail::recompute_dijkstra(
-        graph_.vertex_count(), source_, dist_,
+        vertex_count(graph_), source_, dist_,
         [&](VertexId u, auto&& relax) {
-          graph_.for_each_neighbor(u, [&](VertexId v, EdgeWeight w) { relax(v, w); });
+          for_each_weighted_neighbor(graph_, u, [&](VertexId v, auto w) {
+            relax(v, static_cast<std::uint64_t>(w));
+          });
         });
   }
 
  private:
   void relax_from_updates(const WeightedUpdateBatch& batch) {
-    if (dist_.size() < graph_.vertex_count()) dist_.resize(graph_.vertex_count(), kInf);
+    if (dist_.size() < vertex_count(graph_)) dist_.resize(vertex_count(graph_), kInf);
 
     using Item = std::pair<std::uint64_t, VertexId>;
     std::priority_queue<Item, std::vector<Item>, std::greater<Item>> queue;
@@ -64,7 +68,7 @@ class IncrementalWeightedSSSP {
           queue.push({candidate, op.dst});
         }
       }
-      if (!graph_.directed() && dist_[op.dst] != kInf && op.weight <= kInf - dist_[op.dst]) {
+      if (!is_directed(graph_) && dist_[op.dst] != kInf && op.weight <= kInf - dist_[op.dst]) {
         const auto reverse_candidate = dist_[op.dst] + op.weight;
         if (reverse_candidate < dist_[op.src]) {
           dist_[op.src] = reverse_candidate;
@@ -76,13 +80,17 @@ class IncrementalWeightedSSSP {
     incremental_detail::propagate_dijkstra(
         dist_, queue,
         [&](VertexId u, auto&& relax) {
-          graph_.for_each_neighbor(u, [&](VertexId v, EdgeWeight w) { relax(v, w); });
+          for_each_weighted_neighbor(graph_, u, [&](VertexId v, auto w) {
+            relax(v, static_cast<std::uint64_t>(w));
+          });
         });
   }
 
-  WeightedDynamicGraph& graph_;
+  Graph& graph_;
   VertexId source_;
   std::vector<std::uint64_t> dist_;
 };
+
+using IncrementalWeightedSSSP = BasicIncrementalWeightedSSSP<WeightedDynamicGraph>;
 
 }  // namespace velographx
